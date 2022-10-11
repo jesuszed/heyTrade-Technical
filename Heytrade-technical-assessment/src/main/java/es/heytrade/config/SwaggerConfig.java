c4a1233f2195f96f8b61f1860a1e33e331363ca3
jrodriguez5 <jrodriguez5@ES-CBMF893.getronics.int-it.net>
2022-10-11 10:57:55 +0200

    Added Swagger Configuration

--- Heytrade-technical-assessment/src/main/java/es/heytrade/config/SwaggerConfig.java
+++ Heytrade-technical-assessment/src/main/java/es/heytrade/config/SwaggerConfig.java
@@ -1,10 +1,11 @@
 package es.heytrade.config;
 
-import java.util.Collections;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,30 +14,30 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
-public class SwaggerConfig {
+@EnableWebMvc
+public class SwaggerConfig implements WebMvcConfigurer {
 
-	static final String detailDescription = "Technical assessment Pokedex for HeyTrade";
+	static final String DETAIL_DESCRIPTION = "Technical assessment Pokedex for HeyTrade";
 
 	@Bean
-	public Docket apiDocket() {
+	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.groupName("moodle")
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("es.heytrade.controllers"))
 				.paths(PathSelectors.any())
 				.build()
-				.apiInfo(getApiInfo());
+				.apiInfo(apiInfoMetaData());
 	}
 
-	private ApiInfo getApiInfo() {
-		return new ApiInfo(
-			"Pokedex API REST",
-			detailDescription,
-			"1.0",
-			"http://www.heytrade.es",
-			new Contact("Heytrade", "https://www.heytrade.es", "contacto@heytrade.es"),
-			"LICENSE",
-			"LICENSE URL",
-			Collections.emptyList());
+	private ApiInfo apiInfoMetaData() {
+
+		return new ApiInfoBuilder().title("Pokedex API REST")
+				.description("Technical assessment Pokedex for Heytrade - Jesús Rodríguez")
+				.contact(new Contact("Dev-Team", "https://www.heytrade.es/", "jesus.zed@gmail.com"))
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+				.version("v1")
+				.build();
 	}
 }
